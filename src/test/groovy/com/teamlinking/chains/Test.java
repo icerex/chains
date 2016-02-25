a80da6456f23a7689bea30dc5c4d9f9abae6c301
rex <397183086@qq.com>
2016-02-25 16:10:55 +0800

    update

--- src/test/groovy/com/teamlinking/chains/Test.java
+++ src/test/groovy/com/teamlinking/chains/Test.java
@@ -10,25 +10,15 @@ import com.teamlinking.chains.common.Constants;
 public class Test {
     public static void main(String args[]){
         System.out.println(Constants.NodeType.text.stack(Constants.NodeType.pic));
-        System.out.println(Constants.NodeType.text.stack(Constants.NodeType.audio));
         System.out.println(Constants.NodeType.text.stack(Constants.NodeType.video));
-        System.out.println(Constants.NodeType.audio.stack(Constants.NodeType.pic));
-        System.out.println(Constants.NodeType.audio.stack(Constants.NodeType.video));
         System.out.println(Constants.NodeType.pic.stack(Constants.NodeType.pic));
-        System.out.println(Constants.NodeType.pic.stack(Constants.NodeType.textAndAudio));
         System.out.println(Constants.NodeType.text.stack(Constants.NodeType.textAndVideo));
-        System.out.println(Constants.NodeType.textAndAudio.stack(Constants.NodeType.textAndVideo));
 
         System.out.println("---------------------");
 
         System.out.println(Constants.NodeType.textAndPic.pop(Constants.NodeType.text));
         System.out.println(Constants.NodeType.textAndPic.pop(Constants.NodeType.pic));
         System.out.println(Constants.NodeType.textAndVideo.pop(Constants.NodeType.video));
-        System.out.println(Constants.NodeType.textAndVideo.pop(Constants.NodeType.audio));
-        System.out.println(Constants.NodeType.textAndAudio.pop(Constants.NodeType.textAndVideo));
-        System.out.println(Constants.NodeType.textAndAudio.pop(Constants.NodeType.textAndPic));
-        System.out.println(Constants.NodeType.audioAndVideo.pop(Constants.NodeType.textAndVideo));
-        System.out.println(Constants.NodeType.audioAndPic.pop(Constants.NodeType.textAndPic));
 
         System.out.println("---------------------");
 
